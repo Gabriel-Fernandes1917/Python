c9d25610c192c536d3525bfec72ce9d13533c96e
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-12 21:03:30 -0300

    class 4
    In this class i learn about types the loop

---
+++ class4/main.py
@@ -0,0 +1,24 @@
+## loop
+
+'''for x in range(100): ## range(size the loop)
+    print(x)
+
+for y in range(90, 100):
+    print(y)'''
+
+##a = int(input('Enter with the number'))
+##div =0
+##for x in range(1, a+1): ## i++ in Python is  i +=1
+##    rest = a % x
+##    if rest == 0:
+##        div = div + 1
+
+##if div == 2:
+##    print('Number {} is primo'.format(a))
+##else:
+##    print('number {} not is primo'.format(a))
+
+a = 0
+while a <= 10:
+    print(a)
+    a +=1
