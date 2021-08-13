893da30f24581dd701439ec98eba75bdea2450ea
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-12 22:33:03 -0300

    update 1.0
    In this update, i add one alert to when user inform onenote larger tham 10.

--- system point the exam/main.py
+++ system point the exam/main.py
@@ -5,7 +5,10 @@ x = int(input('Inform first note: '))
 y = int(input('Inform second note: '))
 print('You need 7 or more points to be approved. ')
 
-if x + y > 14:
-    print('approved with: {}'.format((x+y)/2)+' points.')
+if x <= 10 and y <= 10:
+    if x + y > 14:
+        print('approved with: {}'.format((x+y)/2)+' points.')
+    else:
+        print('You not approved, because you need 7 points and you have {}.'. format((x+y)/2))
 else:
-    print('You not approved, because you need 7 points and you have {}.'. format((x+y)/2))
+    print('some note is wrong')
