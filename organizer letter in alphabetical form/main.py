4acd61418b57806c1b3720eaa1b3b2b1e25c7b19
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-14 22:29:24 -0300

    organizer letters in alphabetical form
    This soft organizer letters in alphabetical form

---
+++ organizer letter in alphabetical form/main.py
@@ -0,0 +1,11 @@
+## This soft organizer letters in alphabetical form
+
+letters = [' ',' ',' ',' ',' ',' ']
+
+for i in range(6):
+    letters[i] = input('inform letter {} \n'.format(i))
+
+letters.sort()
+print(letters)
+
+
