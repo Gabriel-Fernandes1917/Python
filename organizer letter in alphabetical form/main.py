d23c6df7443c5b4c1525ae624d7db737bb9fddc9
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-15 22:57:04 -0300

    Update 2.0
    In this update the user can choose 2 forms the see your list.

--- organizer letter in alphabetical form/main.py
+++ organizer letter in alphabetical form/main.py
@@ -3,9 +3,23 @@
 names = [' ',' ',' ',' ',' ',' ']
 
 for i in range(6):
-    names[i] = input('inform name {} \n'.format(i+1))
+    names[i] = input('inform names {} \n'.format(i+1))
 
 names.sort()
-print(names)
+##choice form
+print('What form you wait see your list ? \n 1- Array \n 2- List \n')
+choice = int(input('inform your option \n'))
+
+if choice == 1:
+    print('The list names organizer is: \n')
+    print(names)
+elif choice == 2:
+    print('The list names organizer is: \n')
+    for i in range(6):
+        print('{}-{}'.format(i+1 ,names[i]))
+else:
+    print('This option not exist')
+
+
 
 
