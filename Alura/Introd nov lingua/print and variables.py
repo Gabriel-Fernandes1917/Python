d21cee48c06add3d56b7ccbf7d7ab43cc79ade68
Gabriel Fernandes <gabgui2001@gmail.com>
2021-09-25 10:53:20 -0300

    1 cap

---
+++ Alura/Introd nov lingua/print and variables.py
@@ -0,0 +1,10 @@
+##print("brazil","win",  5, "cup", sep="-") ## sep=""  add paramts between elements.
+
+##print("brazil","win",  5, "cup", end="END") ## not break line in the end
+
+pais = "Russia"
+##print(type(pais))
+quantidade = 4
+##print(type(quantidade))
+
+print(pais, "win", quantidade , "cups")
